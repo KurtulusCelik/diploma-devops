13979d7d9e96e2a92dca140be1a16c1db171fc8f
KurtulusCelik <2023260@student.cct.ie>
2025-07-21 15:14:11 +0100

    feat: added new endpoint for book creation

--- api/views.py
+++ api/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from .serializers import BookSerializer
+from .models import Book
 
 class HealthView(APIView):
     def get(self, request, *args, **kwargs):
@@ -12,12 +15,19 @@ class HealthView(APIView):
     
 health_view = HealthView.as_view()    
 
+#/api/books - All methods (GET, POST)
 class BookView(APIView):
     def get(self, request, *args, **kwargs):
-        return Response(
-            {
-                "hello": "django"
-            }
-        )
+        all_books = Book.objects.all()
+        serializer = BookSerializer(all_books, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request, *args, **kwargs):
+        data = request.data
+
+        serializer = BookSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
     
 book_view = BookView.as_view()    
